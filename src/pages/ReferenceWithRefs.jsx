b5a9b8b72dd008f9d7873c89d88710b28a13691f
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-12 15:18:53 +0600

    Update ReferenceWithRefs file

--- src/pages/ReferenceWithRefs.jsx
+++ src/pages/ReferenceWithRefs.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const ReferenceWithRefs = () => {
+    let ref = useRef(0)
+
+    const showAlert = () => {
+        ref.current = ref.current + 1
+        alert('You clicked ' + ref.current + ' times')
+    }
+
     return (
         <>
             <h2 className="card-title text-2xl mb-4">
                 Referencing values with Refs
             </h2>
+            <button className='btn btn-primary w-40' onClick={() => showAlert()}>
+                Click Here
+            </button>
         </>
     )
 }
