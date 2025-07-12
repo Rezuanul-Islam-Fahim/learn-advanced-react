358fd7283d4af431c034699846e8c077522cb284
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-12 19:00:13 +0600

    Update page reference-with-refs

--- src/pages/ReferenceWithRefs.jsx
+++ src/pages/ReferenceWithRefs.jsx
@@ -1,11 +1,30 @@
-import React, { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 const ReferenceWithRefs = () => {
-    let ref = useRef(0)
+    const ref = useRef(0)
+    const [startTime, setStartTime] = useState(null)
+    const [endTime, setEndTime] = useState(null)
+    const timerRef = useRef(null)
 
     const showAlert = () => {
         ref.current = ref.current + 1
         alert('You clicked ' + ref.current + ' times')
+    }
+
+    const startTimer = () => {
+        setStartTime(Date.now())
+        setEndTime(Date.now())
+
+        timerRef.current = setInterval(() => setEndTime(Date.now()), 50)
+    }
+
+    const stopTimer = () => {
+        clearInterval(timerRef.current)
+    }
+
+    let elapsedTime = 0;
+    if (startTime && endTime) {
+        elapsedTime = (endTime - startTime) / 1000
     }
 
     return (
@@ -13,9 +32,19 @@ const ReferenceWithRefs = () => {
             <h2 className="card-title text-2xl mb-4">
                 Referencing values with Refs
             </h2>
-            <button className='btn btn-primary w-40' onClick={() => showAlert()}>
+            <button className="btn btn-primary w-40" onClick={() => showAlert()}>
                 Click Here
             </button>
+            <br />
+            <p>Time Passed: {elapsedTime.toFixed(3)}</p>
+            <div className="flex flex-row">
+                <button className="btn btn-primary" onClick={startTimer}>
+                    Start Timer
+                </button>
+                <button className="btn btn-error ml-3" onClick={stopTimer}>
+                    End Timer
+                </button>
+            </div>
         </>
     )
 }
